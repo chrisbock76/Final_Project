2812bb9439d8e3a9598b3e7bc74e776300967564
Frankie <frankie710@gmail.com>
2020-08-04 13:27:35 -0400

    anova-on-page

--- assets/analysis/analysis.js
+++ assets/analysis/analysis.js
@@ -1,5 +1,6 @@
 
 
+
 d3.csv("panel_uniprot.csv").then((data) => {
 
     var panelData = data
@@ -53,6 +54,7 @@ function proteinDropdown() {
 }
 
 function showProteinData(protein) {
+    
     d3.csv("protein_scraped_data.csv").then((data) => {
         var proteinInfo = data.filter(data => data["Uniprot ID"] == protein);
         //console.log(protein)
@@ -66,12 +68,15 @@ function showProteinData(protein) {
         name.text(proteinInfo[0]["Protein Name"])
         gene.text(proteinInfo[0]["Gene Name"])
         func.text(proteinInfo[0]["Function"])
+        var gene_name = proteinInfo[0]["Gene Name"]
+        buildBoxPlotTime(protein, gene_name)
+        Sitebp(protein, gene_name)
     })
-    buildBoxPlotTime(protein)
-    Sitebp(protein)
+
+
 }
 
-function buildBoxPlotTime(uniprot) {
+function buildBoxPlotTime(uniprot, gene) {
     Promise.all([
         d3.csv("responser.csv"),
         d3.csv("nonresponser.csv"),
@@ -123,7 +128,7 @@ function buildBoxPlotTime(uniprot) {
         var trace1 = {
             y: baseline_R,
             name: 'BaseLine',
-            marker: { color: '#3D9970' },
+            marker: { color: 'blue' },
             legendgroup:'Responder',
             showlegend: false,
             type: 'box'
@@ -131,7 +136,7 @@ function buildBoxPlotTime(uniprot) {
         var trace2 = {
             y: six_R,
             name: 'Six Weeks',
-            marker: { color: '#3D9970' },
+            marker: { color: 'blue' },
             legendgroup: 'Responder',
             showlegend: false,
             type: 'box'
@@ -139,7 +144,7 @@ function buildBoxPlotTime(uniprot) {
         var trace3 = {
             y: twelve_R,
             name: 'Twelve Weeks',
-            marker: { color: '#3D9970' },
+            marker: { color: 'blue' },
             legendgroup: 'Responder',
             showlegend: false,
             type: 'box'
@@ -148,7 +153,7 @@ function buildBoxPlotTime(uniprot) {
         var trace4 = {
             y: baseline_N,
             name: 'BaseLine',
-            marker: { color: '##FF4136' },
+            marker: { color: 'red' },
             legendgroup: 'Nonresponder',
             showlegend: false,
             type: 'box'
@@ -156,7 +161,7 @@ function buildBoxPlotTime(uniprot) {
         var trace5 = {
             y: six_N,
             name: 'Six Weeks',
-            marker: { color: '##FF4136' },
+            marker: { color: 'red' },
             legendgroup: 'Nonresponder',
             showlegend: false,
             type: 'box'
@@ -164,7 +169,7 @@ function buildBoxPlotTime(uniprot) {
         var trace6 = {
             y: twelve_N,
             name: 'Twelve Weeks',
-            marker: { color: '##FF4136' },
+            marker: { color: 'red' },
             legendgroup: 'Nonresponder',
             showlegend: false,
             type: 'box'
@@ -174,7 +179,9 @@ function buildBoxPlotTime(uniprot) {
         var boxData = [trace1, trace2, trace3, trace4, trace5, trace6];
 
         var layout = {
+            title: 'Protein Levels at Timepoints',
             yaxis: {
+                title: "NPX",
                 zeroline: false
             },
             boxmode: 'group'
@@ -186,6 +193,107 @@ function buildBoxPlotTime(uniprot) {
     }).catch(function (err) {
         throw err
     })
+
+    findAnovaR(gene)
+    findAnovaNR(gene)
+}
+
+function findAnovaR(name) {
+    d3.csv("../../anova_data/anova_R.csv").then((data) => {
+        console.log(name)
+        var tableDiv = d3.select("#anova_R")
+        var tableTitle = d3.select("#table_title_R")
+        tableDiv.style('display', 'block')
+        tableTitle.text(`ANOVA Statistics for ${name}: Responder`)
+        var table = tableDiv.select("table")
+        var tbody = table.select("tbody")
+        tbody.html("")
+        var anovaInfo = data.filter(data => data["Protein"].split("_")[0] == name);
+        //console.log(anovaInfo)
+        var c_f = anovaInfo[0]["C(Time): F"]
+        var c_PR = anovaInfo[0]["C(Time): PR(>F)"]
+        var c_df = anovaInfo[0]["C(Time): df"]
+        var c_sum_sq = anovaInfo[0]["C(Time): sum_sq"]
+        var r_df = anovaInfo[0]["Residual: df"]
+        var r_sum_sq = anovaInfo[0]["Residual: sum_sq"]
+        var table_data_ob = [{
+            aa: "C(Time)",
+            bb: c_sum_sq,
+            cc: c_df,
+            dd: c_f,
+            ee: c_PR
+        },
+        {
+            aa: "Residual",
+            bb: r_sum_sq,
+            cc: r_df,
+            dd: ' ',
+            ee: ' '
+        }
+        ]
+        var table_data = ["C(Site)", c_f, c_PR, c_df, c_sum_sq, "Residual", r_df, r_sum_sq, ' ', ' ']
+        var table_data_1 = ["C(Site)", c_f, c_PR, c_df, c_sum_sq]
+        var table_data_2 = ["Residual", r_df, r_sum_sq, ' ', ' ']
+        //console.log(`c_f: ${c_f}`)
+        //console.log(`c_PR: ${c_PR}`)
+        //console.log(`c_df: ${c_df}`)
+        //console.log(`c_sum_sq: ${c_sum_sq}`)
+        //console.log(`r_df: ${r_df}`)
+        //console.log(`r_sum_sq: ${r_sum_sq}`)
+        var tbody = tbody
+        .selectAll("tr")
+        .data(table_data_ob)
+        .enter()
+        .append("tr")
+        .html(function(d) {
+            return `<td>${d.aa}</td><td>${d.bb}</td><td>${d.cc}</td><td>${d.dd}</td><td>${d.ee}</td>`;
+          });
+
+})
+}
+
+function findAnovaNR(name) {
+    d3.csv("../../anova_data/anova_NR.csv").then((data) => {
+        var tableDiv = d3.select("#anova_NR")
+        var tableTitle = d3.select("#table_title_NR")
+        tableDiv.style('display', 'block')
+        tableTitle.text(`ANOVA Statistics for ${name}: Non-Responder`)
+        var table = tableDiv.select("table")
+        var tbody = table.select("tbody")
+        tbody.html("")
+        var anovaInfo = data.filter(data => data["Protein"].split("_")[0] == name);
+        var c_f = anovaInfo[0]["C(Time): F"]
+        var c_PR = anovaInfo[0]["C(Time): PR(>F)"]
+        var c_df = anovaInfo[0]["C(Time): df"]
+        var c_sum_sq = anovaInfo[0]["C(Time): sum_sq"]
+        var r_df = anovaInfo[0]["Residual: df"]
+        var r_sum_sq = anovaInfo[0]["Residual: sum_sq"]
+        var table_data_ob = [{
+            aa: "C(Time)",
+            bb: c_sum_sq,
+            cc: c_df,
+            dd: c_f,
+            ee: c_PR
+        },
+        {
+            aa: "Residual",
+            bb: r_sum_sq,
+            cc: r_df,
+            dd: ' ',
+            ee: ' '
+        }
+        ];
+
+        var tbody = tbody
+        .selectAll("tr")
+        .data(table_data_ob)
+        .enter()
+        .append("tr")
+        .html(function(d) {
+            return `<td>${d.aa}</td><td>${d.bb}</td><td>${d.cc}</td><td>${d.dd}</td><td>${d.ee}</td>`;
+          });
+
+})
 }
 
 
@@ -196,7 +304,7 @@ function findUniprotValues(uniprotID, array, valuearray) {
     }
 }
 
-function Sitebp(uniprot){
+function Sitebp(uniprot, gene){
     d3.csv("../../Resources/data1+info.csv").then(function (data) {
         var baseline_array = {
             values: [],
@@ -248,7 +356,7 @@ function Sitebp(uniprot){
     var trace3 = {
         y: siteC,
         name: 'Site_C',
-        marker: { color: 'yellow' },
+        marker: { color: 'orange' },
         type: 'box'
     };
 
@@ -268,7 +376,9 @@ function Sitebp(uniprot){
     var boxData = [trace1, trace2, trace3, trace4, trace5];
 
     var layout = {
+        title: 'Protein Levels by Sites at Baseline',
         yaxis: {
+            title: "NPX",
             zeroline: false
         },
         boxmode: 'group'
@@ -279,5 +389,49 @@ function Sitebp(uniprot){
 
     })
 
+    findAnovaS(gene)
 }
 
+function findAnovaS(name) {
+    d3.csv("../../anova_data/anova_site.csv").then((data) => {
+        var tableDiv = d3.select("#anova_S")
+        var tableTitle = d3.select("#table_title_S")
+        tableDiv.style('display', 'block')
+        tableTitle.text(`ANOVA Statistics for ${name}: Site Difference`)
+        var table = tableDiv.select("table")
+        var tbody = table.select("tbody")
+        tbody.html("")
+        var anovaInfo = data.filter(data => data["Protein"].split("_")[0] == name);
+        var c_f = anovaInfo[0]["C(Site): F"]
+        var c_PR = anovaInfo[0]["C(Site): PR(>F)"]
+        var c_df = anovaInfo[0]["C(Site): df"]
+        var c_sum_sq = anovaInfo[0]["C(Site): sum_sq"]
+        var r_df = anovaInfo[0]["Residual: df"]
+        var r_sum_sq = anovaInfo[0]["Residual: sum_sq"]
+        var table_data_ob = [{
+            aa: "C(Site)",
+            bb: c_sum_sq,
+            cc: c_df,
+            dd: c_f,
+            ee: c_PR
+        },
+        {
+            aa: "Residual",
+            bb: r_sum_sq,
+            cc: r_df,
+            dd: ' ',
+            ee: ' '
+        }
+        ];
+
+        var tbody = tbody
+        .selectAll("tr")
+        .data(table_data_ob)
+        .enter()
+        .append("tr")
+        .html(function(d) {
+            return `<td>${d.aa}</td><td>${d.bb}</td><td>${d.cc}</td><td>${d.dd}</td><td>${d.ee}</td>`;
+          });
+
+})
+}
